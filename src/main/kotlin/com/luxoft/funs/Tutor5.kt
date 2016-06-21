e5d621bf28da711e8564dcace4e4f6665ff8592b
Bartek Kuczynski <BKuczynski@luxoft.com>
2016-06-21 15:35:22 +0200

    Update tutor 5

--- src/main/kotlin/com/luxoft/funs/Tutor5.kt
+++ src/main/kotlin/com/luxoft/funs/Tutor5.kt
@@ -1,8 +1,7 @@
 package com.luxoft.funs
 
 /**
- * Write recursive version of GCD:
  *
- * # if a mod b == 0 return b
- * # return gcd (b , a mod b).
+ * Write recursive version of factorial.
+ *
  */
