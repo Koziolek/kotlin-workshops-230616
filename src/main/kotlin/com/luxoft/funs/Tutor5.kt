b173b8c1eba7a4f7ca7b1a7d06890eda3ff2dd2f
Bartek Kuczynski <BKuczynski@luxoft.com>
2016-06-22 11:28:31 +0200

    Solution of tutor 5

--- src/main/kotlin/com/luxoft/funs/Tutor5.kt
+++ src/main/kotlin/com/luxoft/funs/Tutor5.kt
@@ -5,3 +5,15 @@ package com.luxoft.funs
  * Write recursive version of factorial.
  *
  */
+
+fun fact(i: Long): Long {
+
+    tailrec fun fact(acc: Long, n: Long): Long {
+        return when (n) {
+            0L -> 1L
+            else -> fact(acc * n, n - 1)
+        }
+    }
+
+    return fact(1, i)
+}
